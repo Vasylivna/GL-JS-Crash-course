40cea8f9026c5f58d593967a21819c744b900dce
Khrystyna Vysidalko <khtystyavysidalko@gmail.com>
2021-11-03 18:02:29 +0200

    GL_JS_Crash 2

---
+++ Test.js
@@ -0,0 +1,6 @@
+console.log("Hello my test ")
+let UserName = "Khrystyna";
+let UserSecondName = "Vysidalko";
+let Age1 = 27;
+console.log(`My name is ${UserName} ${UserSecondName}`);
+console.log(`My age is  ${Age1}`);
